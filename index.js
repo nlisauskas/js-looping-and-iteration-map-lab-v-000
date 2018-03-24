44565b914dcecf31e762f1a114e34753c43b551e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-24 15:31:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file.
-function lowerCaseDrivers(drivers) {
-  drivers.map(toLowerCase());
+function lowerCaseDrivers (list) {
+  return list.map(function (driver) {
+    return driver.toLowerCase();
+  })
 }
