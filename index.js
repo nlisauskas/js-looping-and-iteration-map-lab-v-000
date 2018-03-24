7ae2f9e1f71d1312f16f62bd94d0d11d160fd26b
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-24 15:34:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function lowerCaseDrivers (list) {
   })
 }
 
-function nameToAttributes() {
+function nameToAttributes(list) {
   return list.map(function (driver) {
     const driverFirst = driver.split(' ')[0]
     const driverLast = driver.split(' ')[1]
